9472ce9c4133698d47c3c060e2a317799290ec5d
GMalvestiti <gustavo.smalvestiti@gmail.com>
2026-05-03 17:17:59 -0300

    [feat] initial commit
    Signed-off-by: GMalvestiti <gustavo.smalvestiti@gmail.com>

---
+++ src/main/java/net/riser876/blockrestore/BlockRestore.java
@@ -0,0 +1,10 @@
+package net.riser876.blockrestore;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BlockRestore implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
