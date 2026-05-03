f2b9e712306d58f44f9ddf8b0a38595568b5287d
GMalvestiti <gustavo.smalvestiti@gmail.com>
2026-05-03 17:39:00 -0300

    [feat] build script and config

--- src/main/java/net/riser876/blockrestore/BlockRestore.java
+++ src/main/java/net/riser876/blockrestore/BlockRestore.java
@@ -1,10 +1,38 @@
 package net.riser876.blockrestore;
 
 import net.fabricmc.api.ModInitializer;
+import net.riser876.blockrestore.config.ConfigManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
+
+import static net.riser876.blockrestore.config.ConfigManager.CONFIG;
 
 public class BlockRestore implements ModInitializer {
 
+    public static final String MOD_ID = "blockrestore";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     @Override
     public void onInitialize() {
+        try {
+            ConfigManager.loadConfig();
+            LOGGER.info("[BlockRestore] Configuration loaded.");
+        } catch (Exception e) {
+            LOGGER.error("[BlockRestore] Failed to load configuration.", e);
+        }
+
+        if (Objects.isNull(CONFIG)) {
+            LOGGER.error("[BlockRestore] Configuration file is broken.");
+            return;
+        }
+
+        if (!CONFIG.ENABLED) {
+            LOGGER.info("[BlockRestore] Mod disabled via config.");
+            return;
+        }
+
+        LOGGER.info("[BlockRestore] Mod initialized.");
     }
 }
---
+++ src/main/java/net/riser876/blockrestore/config/Config.java
@@ -0,0 +1,10 @@
+package net.riser876.blockrestore.config;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Config {
+
+    @Expose @SerializedName("enabled")
+    public boolean ENABLED = true;
+}
